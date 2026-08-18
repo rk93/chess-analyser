48036ccccaecdac6efab8bc30809dd0bc9fa1cda
Ravi Kiran G <rkiran457@gmail.com>
2026-08-18 22:49:45 +0100

    Bump cache for drag and drop analysis board

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chess-analyser-pages-v18';
+const CACHE='chess-analyser-pages-v19';
 const CORE=['./','./index.html','./styles.css','./fixes.css','./app.js','./enhancements.js','./lab.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
